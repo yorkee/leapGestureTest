e1d0fa24634d97cac58d86a9daf90181c7f987d2
Elvin Yeung <elvinyeung@gmail.com>
2018-03-13 00:01:48 -0400

    test file

--- public/leap.js
+++ public/leap.js
@@ -1,46 +1,46 @@
-var controller = new Leap.Controller({enableGestures: true});
+// var controller = new Leap.Controller({enableGestures: true});
 
-controller.on('gesture', function (gesture) {
-  switch (gesture.type){
-    case "circle":
-        console.log("Circle Gesture");
-        break;
-    case "keyTap":
-        console.log("Key Tap Gesture");
-        break;
-    case "screenTap":
-        console.log("Screen Tap Gesture");
-        break;
-    case "swipe":
-        console.log("Swipe Gesture");
-        break;
-  }
-});
-
-
-var controller = Leap.loop({enableGestures: true}, function(frame){
-  //... handle frame data
-});
-
-controller.on("gesture", function(gesture){
-  switch (gesture.type){
-    case "circle":
-        console.log("Circle Gesture");
-        break;
-    case "keyTap":
-        console.log("Key Tap Gesture");
-        break;
-    case "screenTap":
-        console.log("Screen Tap Gesture");
-        break;
-    case "swipe":
-        console.log("Swipe Gesture");
-        break;
-  }
-});
+// controller.on('gesture', function (gesture) {
+//   switch (gesture.type){
+//     case "circle":
+//         console.log("Circle Gesture");
+//         break;
+//     case "keyTap":
+//         console.log("Key Tap Gesture");
+//         break;
+//     case "screenTap":
+//         console.log("Screen Tap Gesture");
+//         break;
+//     case "swipe":
+//         console.log("Swipe Gesture");
+//         break;
+//   }
+// });
 
 
-controller.connect();
+// var controller = Leap.loop({enableGestures: true}, function(frame){
+//   //... handle frame data
+// });
+
+// controller.on("gesture", function(gesture){
+//   switch (gesture.type){
+//     case "circle":
+//         console.log("Circle Gesture");
+//         break;
+//     case "keyTap":
+//         console.log("Key Tap Gesture");
+//         break;
+//     case "screenTap":
+//         console.log("Screen Tap Gesture");
+//         break;
+//     case "swipe":
+//         console.log("Swipe Gesture");
+//         break;
+//   }
+// });
+
+
+// controller.connect();
 
 
 
@@ -120,50 +120,4 @@ console.log("Extended fingers: " + extendedFingers);
   }
 });
 
-
-
-// 	var controller = new Leap.Controller();
-
-// controller.on('connect', function(){
-//    setInterval(function(){
-//       var frame = controller.frame();
-
-//       if(frame.hands.length > 0)
-//       {
-//           var hand = frame.hands[0];
-//           // var position = hand.palmPosition;
-//           // var velocity = hand.palmVelocity;
-//           // var direction = hand.direction;
-
-//           //hand.palmPosition[0]
-//           //hand.palmPosition[1]
-//           //hand.palmPosition[2]
-
-// 					_movePrev.copy( _moveCurr );
-// 					_moveCurr.copy( getMouseOnCircle( hand.palmPosition[0], hand.palmPosition[2]/10 ) );
-          
-//       }
-//    }, 200);
-// });
-// controller.connect();
-
-  // var controller = Leap.loop(function(frame){
-  //     if(frame.hands.length > 0)
-  //     {
-  //         var hand = frame.hands[0];
-  //         // var position = hand.palmPosition;
-  //         // var velocity = hand.palmVelocity;
-  //         // var direction = hand.direction;
-
-  //         //hand.palmPosition[0]
-  //         //hand.palmPosition[1]
-  //         //hand.palmPosition[2]
-
-		// 			_movePrev.copy( _moveCurr );
-		// 			_moveCurr.copy( getMouseOnCircle( hand.palmPosition[0], hand.palmPosition[1] ) );
-          
-  //     }  	
-  // }); 
-
-// controller.connect();
 };
